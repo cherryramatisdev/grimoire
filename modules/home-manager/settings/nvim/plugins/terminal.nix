5e649bdc26f9c430c7c976f7a3c3385329b3fc44
Cherry Ramatis <cherry.ramatis@icloud.com>
2024-02-07 19:58:47 -0300

    use lua

--- modules/home-manager/settings/nvim/plugins/terminal.nix
+++ modules/home-manager/settings/nvim/plugins/terminal.nix
@@ -17,22 +17,17 @@ in
             plugin = toggleterm-nvim;
             type = "lua";
             config = /* lua */''
+                vim.cmd [[
+                " Automatically close terminal when process ends
+                autocmd TermClose * execute 'bdelete! ' . expand('<abuf>')
+                ]]
+
                 require("toggleterm").setup({
                     direction = 'tab'
                 })
 
-                vim.cmd [[
-                    " set
-                    nnoremap <leader>g <cmd>tabnew term://lazygit<cr>
-                    autocmd TermEnter term://*toggleterm#*
-                          \ tnoremap <silent><leader>a <Cmd>exe v:count1 . "ToggleTerm"<CR>
-
-                    " By applying the mappings this way you can pass a count to your
-                    " mapping to open a specific window.
-                    " For example: 2<leader>a will open terminal 2
-                    nnoremap <silent><leader>a <Cmd>exe v:count1 . "ToggleTerm"<CR>
-                    inoremap <silent><leader>a <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
-                ]]
+                vim.keymap.set('n', '<leader>g', '<cmd>tabnew term://lazygit<cr>')
+                vim.keymap.set({'t', 'n', 'i'}, '<leader>a', '<Cmd>exe v:count1 .. "ToggleTerm"<CR>', { silent = true })
             '';
           }
       (fromGitHub "c3d253864784fdb33cc5013b7afc4f0910e2cac3" "master" "nikvdp/neomux")
